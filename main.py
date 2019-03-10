393f3303c4c26c98e5c9094968ed730446e80155
lugo7 <lugo.michael.77@gmail.com>
2019-03-09 21:51:27 -0500

    Output is being displayed through flask

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 #Flask backend
 import os.path
 import time
-from flask import Flask
+import re
+from flask import Flask, render_template
 import createDictDb
 import createDailyDb
 import selectRand1
 app = Flask(__name__)
 
-def splitDef(x):
-    pass
+def splitDef(row):
+    temp=[row[0]]
+    temp.append((row[1].split('.',1)))
+    if(bool(re.search(r'\d', row[1]))==True):
+        temp.append(row[1].split(re.search(r'\d')))
+    return temp
 
-#@app.route("/")
+@app.route("/")
 def displayWord():
-    print('pass')
     if(os.path.isfile('dictionary.db')!=True): #check if the main database file exists.
         createDictDb.main()#if not, create new database.
     if(os.path.isfile('dict24.db')!=True):
         createDailyDb.main() #Creates a database of 24 words.
     row=selectRand1.main() #picks out one random number from dailyDict
-    print(row[0])
-    print(row[1])
-    return row
+    temp=splitDef(row)
+    print(temp)
+    #print(row[0])
+    #print(row[1])
+    return render_template('home.html', word=temp[0] , type=temp[1][0]+'.', definition=temp[1][1])
 
 if __name__ == "__main__":
-    #app.run()
-    displayWord()
+    app.run(debug=True)
+    #displayWord()
